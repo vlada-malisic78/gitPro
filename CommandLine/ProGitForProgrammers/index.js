89949011f79853f4b240338ef760df79c29cba8d
Vlada <vladamalisic78@gmail.com>
2021-11-11 22:51:12 +0100

    create add function

--- CommandLine/ProGitForProgrammers/index.js
+++ CommandLine/ProGitForProgrammers/index.js
@@ -3,3 +3,7 @@ let year = date.getFullYear();
 let number = Math.ceil(Math.random() * 31);
 console.log(`Random number is ${number}`);
 console.log(`This year is ${year}`);
+
+function add(a, b) {
+  return add(5, 3);
+}
