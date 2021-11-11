076ff9e2e42c6cfd28d96f8dd808b1cc1d018a0a
Vlada <vladamalisic78@gmail.com>
2021-11-11 20:10:37 +0100

    Add random number

--- CommandLine/ProGitForProgrammers/index.js
+++ CommandLine/ProGitForProgrammers/index.js
@@ -1,4 +1,5 @@
 let date = new Date();
 let year = date.getFullYear();
-
+let number = Math.ceil(Math.random() * 31);
+console.log(`Random number is ${number}`);
 console.log(`This year is ${year}`);
