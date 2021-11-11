c0a0308578fe88bdfa88b0c2e8769484dc7fb494
Vlada <vladamalisic78@gmail.com>
2021-11-11 20:12:05 +0100

    Add my lucky number

--- CommandLine/ProGitForProgrammers/index.js
+++ CommandLine/ProGitForProgrammers/index.js
@@ -1,4 +1,5 @@
 let date = new Date();
 let year = date.getFullYear();
-
+let number = Math.ceil(Math.random() * 100);
+console.log(`My number is : ${number}`);
 console.log(`This year is ${year}`);
